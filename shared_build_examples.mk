d286357048ff6d1b7995d8314333c51a24c40b34
Delano Flipse <delanoflipse@hotmail.com>
2025-04-04 15:52:31 +0200

    chore: update compose files, seperate make steps

--- shared_build_examples.mk
+++ shared_build_examples.mk
@@ -71,8 +71,10 @@ docker-build: generate-protos
 	AWS_ACCOUNT_ID=$(AWS_ACCOUNT_ID) REGION=$(REGION) docker-compose build
 	docker pull jaegertracing/all-in-one:1.6
 
-docker-start:
+docker-build:
 	DISABLE_INSTRUMENTATION=$(DISABLE_INSTRUMENTATION) make docker-build
+
+docker-start:
 	AWS_ACCOUNT_ID=$(AWS_ACCOUNT_ID) REGION=$(REGION) docker-compose up -d
 
 docker-stop:
